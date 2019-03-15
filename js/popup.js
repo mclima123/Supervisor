551b677232768ca240ad5c7b36bd34b553b7eeb2
Marco Lima <marcopm1997@hotmail.com>
2019-03-15 13:22:25 +0000

    Initial popup layout.

--- js/popup.js
+++ js/popup.js
@@ -1,25 +1,21 @@
-let changeColor = document.getElementById('changeColor');
+//mudar ests funções do tutorial
+//alert("teste");
 
-chrome.storage.sync.get('color', function (data) {
-  changeColor.style.backgroundColor = data.color;
-  changeColor.setAttribute('value', data.color);
-});
-
-changeColor.onclick = function (element) {
-  let color = element.target.value;
-  chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-    chrome.tabs.executeScript(
-      tabs[0].id,
-      { code: 'document.body.style.backgroundColor = "' + color + '";' });
-  });
-};
-
-
+// --------- LINKS ÚTEIS --------- //
 /*
 https://stackoverflow.com/questions/8894461/updating-an-extension-button-dynamically-inspiration-required
 -mudar o icon da extensão consoante o site e tempo em que está no site
 
 https://scotch.io/@dongido/how-to-build-a-google-chrome-extension
 -css and stuff
+
+https://stackoverflow.com/questions/44712495/what-are-the-differences-between-page-action-and-browser-action
+-browser actions vs page actions
+
+https://developer.chrome.com/extensions/browserAction
+-browser action
+
+https://www.kirupa.com/html5/dynamically_create_populate_list.htm
+-populate list
 
 */
