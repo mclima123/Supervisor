2eec960c57f73aa356d051aecc49060ce7734b36
Marco Lima <marcopm1997@hotmail.com>
2019-03-15 18:43:08 +0000

    Progress?

--- js/popup.js
+++ js/popup.js
@@ -1,5 +1,53 @@
-//mudar ests funções do tutorial
-//alert("teste");
+let blacklist = [];
+getBlacklist();
+
+document.getElementById('add_to_blacklist').addEventListener("click", addToBlacklist);
+document.getElementById('remove_from_blacklist').addEventListener("click", removeFromBlacklist);
+document.getElementById('title').addEventListener("click", clearStorage);
+
+function addToBlacklist() {
+  let website = document.getElementById('website_input').value;
+
+  if (!blacklist.includes(website)) {
+    let list = document.getElementById('append_here');
+    let child = document.createElement("div");
+    child.innerHTML = "<li class='list-group-item list-group-item-success'><div id='website_name'>" + website + "</div><button id='removeButton'>remove</button></li>"
+    blacklist.push(website);
+    saveBlacklist();
+    list.append(child);
+    //console.log('teste');
+  }
+}
+
+function removeFromBlacklist() {
+
+}
+
+
+
+
+
+
+//--------------------------//
+
+function getBlacklist() {
+  let aux = localStorage.getItem('blacklist');
+  blacklist = aux == null ? blacklist : JSON.parse(aux);
+
+  console.log(blacklist);
+}
+
+function clearStorage() {
+  localStorage.clear();
+  blacklist = [];
+}
+
+function saveBlacklist() {
+  localStorage.setItem('blacklist', JSON.stringify(blacklist));
+}
+
+//--------------------------//
+
 
 // --------- LINKS ÚTEIS --------- //
 /*
@@ -17,5 +65,8 @@ https://developer.chrome.com/extensions/browserAction
 
 https://www.kirupa.com/html5/dynamically_create_populate_list.htm
 -populate list
+
+https://stackoverflow.com/questions/12395722/can-the-window-object-be-modified-from-a-chrome-extension
+-access parent window
 
 */
