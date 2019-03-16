e2ba6fad718fbe2516c0123dd1c9d4d1f8e266ef
Marco Lima <marcopm1997@hotmail.com>
2019-03-16 20:51:42 +0000

    modal in progress

--- js/background.js
+++ js/background.js
@@ -8,59 +8,44 @@ var t = setInterval(runEverySecond, 1000);
   getAllOpenWindows(window);
 });*/
 
-function runEverySecond(){
+function runEverySecond() {
   chrome.tabs.query({ currentWindow: true, active: true }, function (tabs) {
     console.log("tabs: " + tabs)
-    if(tabs.length != 0){
+    if (tabs.length != 0) {
       isBlacklistedUrlOpen(tabs[0])
     }
     console.log("timer: " + timer);
   })
 }
 
-
 //confirmed this changes the icon
 chrome.browserAction.setIcon({
   path: "images/green_128.png" //any icon in the folder
 });
 
-chrome.runtime.onInstalled.addListener(function () {
-  chrome.declarativeContent.onPageChanged.removeRules(undefined, function () {
-    chrome.declarativeContent.onPageChanged.addRules([{
-      conditions: [new chrome.declarativeContent.PageStateMatcher({
-        pageUrl: {
-          //add social media sites here
-          urlMatches: '(facebook|instagram|twitter).com',
-        },
-      })
-      ],
-      actions: [new chrome.declarativeContent.ShowPageAction()]
-    }]);
-  });
+if (timer == MAX_TIME) {
+  alert("test boi");
+  timer -= MAX_TIME; // timer = 0 doesn't reset it god bless
+}
 
-  if(timer == MAX_TIME)
-  {
-    alert("test boi");
-    timer -= MAX_TIME; // timer = 0 doesn't reset it god bless
-  }
-})
-
-function isBlacklistedUrlOpen(current_tab){
+function isBlacklistedUrlOpen(current_tab) {
   let blacklist = []
   let aux = localStorage.getItem('blacklist')
   blacklist = aux == null ? blacklist : JSON.parse(aux)
 
-  for(let i=0; i<blacklist.length; i++)
-  {
+  for (let i = 0; i < blacklist.length; i++) {
     console.log("site: " + blacklist[i]);
     console.log("current: " + current_tab.url);
-    if(current_tab.url === blacklist[i])
+    if (current_tab.url === blacklist[i])
       timer++;
   }
 }
 
-function getURLDomain(url){
+function getURLDomain(url) {
   var splits = url.split("://")       // http + <rest of the url>
   var splits2 = splits[1].split("/")  // site.com + page identifier
   return splits[0] + "://" + splits2[0];
 }
+
+
+//----------------------------------------------------------------------------//
