7a92bba1f4677c8e9124dd82a4a894fc57472186
Marco Lima <marcopm1997@hotmail.com>
2019-03-16 17:10:38 +0000

    changed icons

--- js/background.js
+++ js/background.js
@@ -1,6 +1,11 @@
 //confirmed este alerta aparece a meio da janela
 //window.alert("teste");
 
+//confirmed this changes the icon
+chrome.browserAction.setIcon({
+  path: "images/green_128.png" //any icon in the folder
+});
+
 chrome.runtime.onInstalled.addListener(function () {
   chrome.declarativeContent.onPageChanged.removeRules(undefined, function () {
     chrome.declarativeContent.onPageChanged.addRules([{
